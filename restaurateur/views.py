249b9b16e98578c262323f373a68bfc676574fb2
svg83 <svgeneraloff@gmail.com>
2024-02-21 11:55:25 +0400

    List orders into manager UI

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
 
 
-from foodcartapp.models import Product, Restaurant
+from foodcartapp.models import Product, Restaurant, Order, Order_details
 
 
 class Login(forms.Form):
@@ -89,9 +89,22 @@ def view_restaurants(request):
         'restaurants': Restaurant.objects.all(),
     })
 
+def orders_for_manager(item):
+    return {
+        'id': item.id,
+        'lastname': item.lastname,
+        'phonenumber': item.phonenumber,
+        'address': item.address,
+    }
+
 
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
-    return render(request, template_name='order_items.html', context={
-        # TODO заглушка для нереализованного функционала
-    })
+    orders = Order.objects.all()
+    context = {
+        'orders': [orders_for_manager(item) for item in orders]
+    }
+    print(context)
+    return render(request,
+                  template_name='order_items.html',
+                  context=context)
