0ddd108b89d1aea34c39538e2ec644b8e981ee38
svgen83 <61458549+svgen83@users.noreply.github.com>
2025-03-09 17:22:12 +0400

    исправлена отправка данных на rollbar

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ set -e
 cd /opt/starburger
 
 git pull
+
 if [ -e "PRG/bin/activate" ]; then
     echo "activating venv"
     source PRG/bin/activate
@@ -24,13 +25,11 @@ systemctl reload nginx.service
 
 systemctl restart burger-shop.service
 
-
 export $(cat /opt/starburger/.env  | grep ROLLBAR_TOKEN | tr -d \')
-export $(cat /opt/starburger/.env  | grep ENVIRONMENT)
-
+export $(cat /opt/starburger/.env  | grep ENVIRONMENT | tr -d \')
 
 GIT_REVISION=$(git rev-parse --short HEAD)
 
-curl -H "X-Rollbar-Access-Token: ${ROLLBAR_TOKEN}" -H "Content-Type: application/json" -H 'accept: application/json' -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "${ENVIRONMENT}", "revision": "${GIT_REVISION}", "comment": "deployment", "status": "succeeded"}'
+curl -H "X-Rollbar-Access-Token: ${ROLLBAR_TOKEN}" -H "Content-Type: application/json" -H 'accept: application/json' -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "'"${ENVIRONMENT}"'", "revision": "'"${GIT_REVISION}"'", "comment": "deployment", "status": "succeeded"}'
 
 echo "Finished"
